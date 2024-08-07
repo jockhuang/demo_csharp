f68063b3c98f32deecd2b6bda343492e870b6807
Jock <jockhuang@gmail.com>
2024-08-08 00:17:42 +1200

    bug fix

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -37,7 +37,8 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: MailListView
-    },{
+    },
+    {
       name: 'AddSubscription',
       path: '/subscriptions/add',
       component: () => import('../views/EditSubscription.vue'),
