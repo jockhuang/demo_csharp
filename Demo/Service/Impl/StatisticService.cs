852ab7fa284cf01f7d2fb85318efae39ac59112f
Jock <jockhuang@gmail.com>
2024-08-08 23:25:30 +1200

    tiny improvement

--- Demo/Service/Impl/StatisticService.cs
+++ Demo/Service/Impl/StatisticService.cs
@@ -15,8 +15,16 @@ public class StatisticService : IStatisticService
 
     public async Task<HomeData> GetHomeData()
     {
-        var releasedCount = await _context.Products.Where(c=>c.IsRelease == true).CountAsync();
-        var unReleasedCount = await _context.Products.Where(c=>c.IsRelease == false).CountAsync();
+        var releasedCount = 0;
+        var unReleasedCount = 0;
+        var products = await _context.Products.GroupBy(c=>c.IsRelease).Select(g => new { name = g.Key, count = g.Count() }).ToListAsync();
+        foreach(var row in products){
+            if(row.name){
+                releasedCount = row.count;
+            }else{
+                unReleasedCount = row.count;
+            }
+        }
         var subscriptions = await _context.MailList.CountAsync();
         return new HomeData{ReleasedProduct=releasedCount, UnReleasedProduct=unReleasedCount, Subscription=subscriptions};
     }
